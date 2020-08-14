d534b7f323af7f710eb44300ddc91164e8318453
user <endrijsss@gmail.com>
2020-08-14 16:49:44 +0300

    20200814_16_49_upload_from_cli

---
+++ Python/diary20200814a.py
@@ -0,0 +1,501 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+Traceback (most recent call last):
+  File "/home/user/EDIBO/Python/ourfirstpythonobject.py", line 10, in <module>
+    Print("After an = PartyAnimal()")
+NameError: name 'Print' is not defined
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+Traceback (most recent call last):
+  File "/home/user/EDIBO/Python/ourfirstpythonobject.py", line 10, in <module>
+    Print("After an = PartyAnimal()")
+NameError: name 'Print' is not defined
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7ff6a82d36a0>}
+Before second an.party
+So far 2
+After second an.party()
+Before third an.party()
+So far 3
+After third an.party()
+
+Before one more party()
+So far 4
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f4ff42bd630>}
+Before second an.party
+So far 101
+After second an.party()
+Before third an.party()
+So far 101
+After third an.party()
+
+Before one more party()
+So far 102
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+an data type or class
+an methods and properties
+an x property data type
+an party method data type
+Before first an.party()
+So far x property of object 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f4020e5b630>}
+Before second an.party
+So far x property of object 101
+After second an.party()
+Before third an.party()
+So far x property of object 101
+After third an.party()
+
+Before one more party()
+So far x property of object 102
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+an data type or class
+an methods and properties
+an x property data type
+an party method data type
+{}
+Before first an.party()
+So far x property of object 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7faadcb166a0>}
+Before second an.party
+So far x property of object 101
+After second an.party()
+Before third an.party()
+So far x property of object 101
+After third an.party()
+
+Before one more party()
+So far x property of object 102
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+an data type or class
+an methods and properties
+an x property data type
+an party method data type
+{}
+Before first an.party()
+So far x property of object 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fe71621a6d8>}
+Before second an.party
+So far x property of object 101
+After second an.party()
+Before third an.party()
+So far x property of object 201
+After third an.party()
+
+Before one more party()
+So far x property of object 202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object 1
+After first an.party
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f290630b6a0>}
+Before second an.party
+So far x property of object 101
+After second an.party()
+Before third an.party()
+So far x property of object 201
+After third an.party()
+
+Before one more party()
+So far x property of object 202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object 1
+After first an.party
+Before second an.party
+So far x property of object 101
+After second an.party()
+Before third an.party()
+So far x property of object 201
+After third an.party()
+
+Before one more party()
+So far x property of object 202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+Traceback (most recent call last):
+  File "/home/user/EDIBO/Python/ourfirstpythonobject.py", line 28, in <module>
+    an.party()
+NameError: name 'an' is not defined
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+I am construced
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+Before second an.party
+So far x property of object is:  101
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> an
+<__main__.PartyAnimal object at 0x7f30249e5668>
+>>> vars(an)
+{'x': 202}
+>>> type(an)
+<class '__main__.PartyAnimal'>
+>>> an.__del__()
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    an.__del__()
+AttributeError: 'PartyAnimal' object has no attribute '__del__'
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+{'x': 0}
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> an.__del__()
+I am destructed 202
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fe52c17f6d8>}
+>>> vars(aqan)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    vars(aqan)
+NameError: name 'aqan' is not defined
+>>> vars(an)
+{'x': 202}
+>>> an
+<__main__.PartyAnimal object at 0x7fe52c17f6d8>
+>>> an = 900
+>>> an
+I am destructed 202
+900
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': 900}
+>>> vars(an)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    vars(an)
+TypeError: vars() argument must have __dict__ attribute
+>>> vars
+<built-in function vars>
+>>> vars(an)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    vars(an)
+TypeError: vars() argument must have __dict__ attribute
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': 900}
+>>> an
+900
+>>> an
+900
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+{'x': 0}
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fb9e8d0c5f8>}
+>>> vars(an)
+{'x': 202}
+>>> an
+<__main__.PartyAnimal object at 0x7fb9e8d0c5f8>
+>>> vars(an)
+{'x': 202}
+>>> x = 900
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fb9e8d0c5f8>, 'x': 900}
+>>> an = 900
+I am destructed 202
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/ourfirstpythonobject.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': 900, 'x': 900}
+>>> an
+900
+>>> type(an)
+<class 'int'>
+>>> vars(an)
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    vars(an)
+TypeError: vars() argument must have __dict__ attribute
+>>> am
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    am
+NameError: name 'am' is not defined
+>>> an
+900
+>>> vars(an)
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    vars(an)
+TypeError: vars() argument must have __dict__ attribute
+>>> 
+========== RESTART: /home/user/EDIBO/Python/ourfirstpythonobject.py ==========
+Before an = PartyAnimal()
+I am construced
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is:  1
+After first an.party
+I am construced
+Before second an.party
+So far x property of object is:  1
+After second an.party()
+Before third an.party()
+So far x property of object is:  201
+After third an.party()
+
+Before one more party()
+So far x property of object is:  202
+After one more party()
+>>> vars(an)
+{'x': 202}
+>>> 
